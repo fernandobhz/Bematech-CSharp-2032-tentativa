d14aa32b8f246d1579f8f168d9bfbe57b5267edf
Fernando Reis Guimarães <fernandobhz@gmail.com>
2020-07-25 18:56:42 -0300

    old project migrated

---
+++ Bematech CSharp 2032 tentativa/Form1.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Bematech_CSharp_2032_tentativa
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int iRetorno;
+            iRetorno = MP2032.IniciaPorta("COM1");
+            iRetorno = MP2032.FormataTX("Bematech – BSP", 2, 0, 0, 1, 1);
+            iRetorno = MP2032.FechaPorta();
+            MessageBox.Show("OK");
+        }
+    }
+}
